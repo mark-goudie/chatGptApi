40de6242083f1b120d697580fb93141ad4bf61c8
Mark Goudie <goudie.mark@gmail.com>
2023-06-13 23:36:26 +1000

    style changes

--- src/client/index.js
+++ src/client/index.js
@@ -26,12 +26,30 @@ document.addEventListener("DOMContentLoaded", () => {
       }),
     };
 
+    // Clear existing interval if there is one
+    if (window.loadingInterval) {
+      clearInterval(window.loadingInterval);
+    }
+
+    // Start loading text
+    responseContainer.innerText = "Loading";
+    let dots = 0;
+
+    // Update the text every 500 milliseconds
+    window.loadingInterval = setInterval(() => {
+      dots = (dots + 1) % 4; // Cycle through 0, 1, 2, 3
+      responseContainer.innerText = "Loading" + ".".repeat(dots);
+    }, 400);
+
     try {
       // Change the URL here
       const response = await fetch(
         "http://localhost:3000/api/chat",
         requestPayload
       );
+
+      // Once response is received, clear the interval
+      clearInterval(window.loadingInterval);
 
       if (!response.ok) {
         const message = await response.text(); // Use .text() instead of .json() to handle non-JSON responses
@@ -40,6 +58,8 @@ document.addEventListener("DOMContentLoaded", () => {
       const data = await response.json();
       responseContainer.innerText = data.choices[0].message.content;
     } catch (err) {
+      // If error occurs, clear the interval
+      clearInterval(window.loadingInterval);
       console.error("Error:", err);
       responseContainer.innerText = "An error occurred. Please try again.";
     }
